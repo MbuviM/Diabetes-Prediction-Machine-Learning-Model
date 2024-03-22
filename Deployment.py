189171102954dd6cc55f5d5ca4916295b14e5b3b
MbuviM <mwendembuvi10@gmail.com>
2024-03-22 13:35:19 +0300

    Updated the model name

--- Deployment.py
+++ Deployment.py
@@ -15,22 +15,23 @@ def predict(input_data):
 st.title("Diabetes Prediction App")
 
 # Input form
-st.sidebar.header("User Input")
-pregnancies = st.sidebar.selectbox("Pregnancies", [0, 1])
-glucose = st.sidebar.slider("Glucose", 0, 200, 100)
-blood_pressure = st.sidebar.slider("Blood Pressure", 0, 150, 80)
-skin_thickness = st.sidebar.slider("Skin Thickness", 0, 99, 20)
-insulin = st.sidebar.slider("Insulin", 0, 900, 100)
-bmi = st.sidebar.slider("BMI", 0.0, 70.0, 25.0)
-diabetes_pedigree_function = st.sidebar.slider("Diabetes Pedigree Function", 0.0, 2.0, 0.5)
-age = st.sidebar.slider("Age", 0, 120, 30)
-gender = st.sidebar.radio("Gender", ["Male", "Female"])
-hypertension = st.sidebar.selectbox("Hypertension", ["Yes", "No"])
-heart_disease = st.sidebar.selectbox("Heart Disease", ["Yes", "No"])
-smoking_history = st.sidebar.selectbox("Smoking History", ["Yes", "No"])
+with st.sidebar:
+    st.header("User Input")
+    pregnancies = st.selectbox("Pregnancies", [0, 1])
+    glucose = st.slider("Glucose", 0, 200, 100)
+    blood_pressure = st.slider("Blood Pressure", 0, 150, 80)
+    skin_thickness = st.slider("Skin Thickness", 0, 99, 20)
+    insulin = st.slider("Insulin", 0, 900, 100)
+    bmi = st.slider("BMI", 0.0, 70.0, 25.0)
+    diabetes_pedigree_function = st.slider("Diabetes Pedigree Function", 0.0, 2.0, 0.5)
+    age = st.slider("Age", 0, 120, 30)
+    gender = st.radio("Gender", ["Male", "Female", "Others"])
+    hypertension = st.selectbox("Hypertension", ["Yes", "No"])
+    heart_disease = st.selectbox("Heart Disease", ["Yes", "No"])
+    smoking_history = st.selectbox("Smoking History", ["Yes", "No"])
 
 # Make prediction on button click
-if st.sidebar.button("Predict"):
+if st.button("Predict"):
     input_data = {
         "Pregnancies": pregnancies,
         "Glucose": glucose,
@@ -47,4 +48,5 @@ if st.sidebar.button("Predict"):
     }
     prediction = predict(input_data)
     st.success(f"The risk of you getting diabetes is {prediction:.2f}%")
+
 
