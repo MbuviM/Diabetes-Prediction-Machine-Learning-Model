6f9e1ca20e6034b5f58f05dbadf41cb9aefa3d05
MbuviM <mwendembuvi10@gmail.com>
2024-03-26 14:49:54 +0300

    Deployment py file

--- Deployment.py
+++ Deployment.py
@@ -12,20 +12,20 @@ model = joblib.load("trained_model.joblib")
 def preprocess_input(input_data):
     # Map gender to numerical value
     gender_map = {"Male": 1, "Female": 2, "Others": 0}
-    input_data["Gender"] = gender_map[input_data["Gender"]]
+    input_data["gender"] = gender_map[input_data["gender"]]
     
     # Map categorical variables to numerical values
     binary_map = {"Yes": 1, "No": 0}
-    input_data["Hypertension"] = binary_map[input_data["Hypertension"]]
-    input_data["HeartDisease"] = binary_map[input_data["HeartDisease"]]
+    input_data["hypertension"] = binary_map[input_data["hypertension"]]
+    input_data["heart_disease"] = binary_map[input_data["heart_disease"]]
     
     # Map smoking history to binary values
     smoking_map = {"never": 0, "No Info": 0, "current": 1, "former": 1, "ever": 1, "not current": 1}
-    input_data["SmokingHistory"] = smoking_map[input_data["SmokingHistory"]]
+    input_data["smoking_history"] = smoking_map[input_data["smoking_history"]]
     
-    return np.array([[input_data[col] for col in ["Pregnancies", "Glucose", "BloodPressure", "SkinThickness",
-                                                  "Insulin", "BMI", "DiabetesPedigreeFunction", "Age",
-                                                  "Gender", "Hypertension", "HeartDisease", "SmokingHistory", "	HbA1c_level"]]])
+    return np.array([[input_data[col] for col in ["gender", "age", "hypertension", "heart_disease",
+                                                  "smoking_history", "bmi", "HbA1c_level",
+                                                  "blood_glucose_level"]]])
 
 # Function to make predictions
 def predict(input_data):
@@ -38,35 +38,26 @@ st.title("Diabetes Prediction App")
 
 # Input form
 st.sidebar.header("User Input")
-pregnancies = st.sidebar.selectbox("Pregnancies", [0, 1])
-glucose = st.sidebar.slider("Glucose", 0, 200, 100)
-blood_pressure = st.sidebar.slider("Blood Pressure", 0, 150, 80)
-skin_thickness = st.sidebar.slider("Skin Thickness", 0, 99, 20)
-insulin = st.sidebar.slider("Insulin", 0, 900, 100)
-bmi = st.sidebar.slider("BMI", 0.0, 70.0, 25.0)
-diabetes_pedigree_function = st.sidebar.slider("Diabetes Pedigree Function", 0.0, 2.0, 0.5)
-age = st.sidebar.slider("Age", 0, 120, 30)
 gender = st.sidebar.radio("Gender", ["Male", "Female", "Others"])
+age = st.sidebar.slider("Age", 0, 120, 30)
 hypertension = st.sidebar.selectbox("Hypertension", ["Yes", "No"])
 heart_disease = st.sidebar.selectbox("Heart Disease", ["Yes", "No"])
 smoking_history = st.sidebar.selectbox("Smoking History", ['never', 'No Info', 'current', 'former', 'ever', 'not current'])
+bmi = st.sidebar.slider("BMI", 0.0, 70.0, 25.0)
+HbA1c_level = st.sidebar.slider("HbA1c Level", 0.0, 15.0, 6.0)
+blood_glucose_level = st.sidebar.slider("Blood Glucose Level", 0, 500, 100)
 
 # Make prediction on button click
 if st.sidebar.button("Predict"):
     input_data = {
-        "Pregnancies": pregnancies,
-        "Glucose": glucose,
-        "BloodPressure": blood_pressure,
-        "SkinThickness": skin_thickness,
-        "Insulin": insulin,
-        "BMI": bmi,
-        "DiabetesPedigreeFunction": diabetes_pedigree_function,
-        "Age": age,
-        "Gender": gender,
-        "Hypertension": hypertension,
-        "HeartDisease": heart_disease,
-        "SmokingHistory": smoking_history
+        "gender": gender,
+        "age": age,
+        "hypertension": hypertension,
+        "heart_disease": heart_disease,
+        "smoking_history": smoking_history,
+        "bmi": bmi,
+        "HbA1c_level": HbA1c_level,
+        "blood_glucose_level": blood_glucose_level
     }
     prediction = predict(input_data)
     st.success(f"The risk of you getting diabetes is {prediction:.2f}%")
-
