7d7110dee68e56e2fa2b3337d26bbf5a43d6e988
MbuviM <mwendembuvi10@gmail.com>
2024-03-26 15:55:18 +0300

    Deployment py file

--- Deployment.py
+++ Deployment.py
@@ -25,9 +25,26 @@ def preprocess_input(input_data):
     smoking_history_map = {"never": 0, "No Info": 0, "current": 1, "former": 1, "ever": 1, "not current": 1}
     input_data["smoking_history"] = smoking_history_map[input_data["smoking_history"]]
     
-    return np.array([[input_data[col] for col in ["gender", "age", "hypertension", "heart_disease",
-                                                  "smoking_history", "bmi", "HbA1c_level",
-                                                  "blood_glucose_level"]]])
+    # Encode categorical variables
+    encoded_gender = [0, 0]  # Initialize encoded gender features
+    if input_data["gender"] == 1:  # Male
+        encoded_gender[0] = 1
+    elif input_data["gender"] == 2:  # Female
+        encoded_gender[1] = 1
+    
+    encoded_smoking_history = [0, 0, 0, 0, 0]  # Initialize encoded smoking history features
+    if input_data["smoking_history"] == "current":
+        encoded_smoking_history[0] = 1
+    elif input_data["smoking_history"] == "former":
+        encoded_smoking_history[1] = 1
+    elif input_data["smoking_history"] == "ever":
+        encoded_smoking_history[2] = 1
+    elif input_data["smoking_history"] == "not current":
+        encoded_smoking_history[3] = 1
+    
+    return np.array([[input_data["gender"], input_data["age"], input_data["hypertension"], input_data["heart_disease"],
+                      input_data["smoking_history"], input_data["bmi"], input_data["HbA1c_level"],
+                      input_data["blood_glucose_level"]] + encoded_gender + encoded_smoking_history])
 
 # Function to make predictions
 def predict(input_data):
@@ -63,6 +80,7 @@ if st.sidebar.button("Predict"):
     }
     prediction = predict(input_data)
     st.success(f"The risk of you getting diabetes is {prediction:.2f}%")
+
 
 
 
