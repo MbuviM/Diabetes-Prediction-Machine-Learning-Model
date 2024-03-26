a133fe7009aa2defac89a839b2e2c85427e49ac1
MbuviM <mwendembuvi10@gmail.com>
2024-03-26 15:50:20 +0300

    Deployment py file

--- Deployment.py
+++ Deployment.py
@@ -25,31 +25,13 @@ def preprocess_input(input_data):
     smoking_history_map = {"never": 0, "No Info": 0, "current": 1, "former": 1, "ever": 1, "not current": 1}
     input_data["smoking_history"] = smoking_history_map[input_data["smoking_history"]]
     
-    # Encode categorical variables
-    encoded_gender = [0, 0]  # Initialize encoded gender features
-    if input_data["gender"] == 1:  # Male
-        encoded_gender[0] = 1
-    elif input_data["gender"] == 2:  # Female
-        encoded_gender[1] = 1
-    
-    encoded_smoking_history = [0, 0, 0, 0, 0]  # Initialize encoded smoking history features
-    if input_data["smoking_history"] == "current":
-        encoded_smoking_history[0] = 1
-    elif input_data["smoking_history"] == "former":
-        encoded_smoking_history[1] = 1
-    elif input_data["smoking_history"] == "ever":
-        encoded_smoking_history[2] = 1
-    elif input_data["smoking_history"] == "not current":
-        encoded_smoking_history[3] = 1
-    
-    return np.array([[input_data["gender"], input_data["age"], input_data["hypertension"], input_data["heart_disease"],
-                      input_data["smoking_history"], input_data["bmi"], input_data["HbA1c_level"],
-                      input_data["blood_glucose_level"]] + encoded_gender + encoded_smoking_history])
+    return np.array([[input_data[col] for col in ["gender", "age", "hypertension", "heart_disease",
+                                                  "smoking_history", "bmi", "HbA1c_level",
+                                                  "blood_glucose_level"]]])
 
 # Function to make predictions
 def predict(input_data):
     input_data_scaled = preprocess_input(input_data)
-    print("Number of features in input data:", input_data_scaled.shape[1])  # Debugging statement
     prediction = model.predict(input_data_scaled)[0][0] * 100  # Predicting probability of class 1 (diabetes)
     return prediction
 
@@ -60,9 +42,9 @@ st.title("Diabetes Prediction App")
 st.sidebar.header("User Input")
 gender = st.sidebar.radio("Gender", ["Male", "Female", "Others"])
 age = st.sidebar.slider("Age", 0, 120, 30)
-hypertension = st.sidebar.radio("Hypertension", ["Yes", "No"])
-heart_disease = st.sidebar.radio("Heart Disease", ["Yes", "No"])
-smoking_history = st.sidebar.radio("Smoking History", ['never', 'No Info', 'current', 'former', 'ever', 'not current'])
+hypertension = st.sidebar.selectbox("Hypertension", ["Yes", "No"])
+heart_disease = st.sidebar.selectbox("Heart Disease", ["Yes", "No"])
+smoking_history = st.sidebar.selectbox("Smoking History", ['never', 'No Info', 'current', 'former', 'ever', 'not current'])
 bmi = st.sidebar.slider("BMI", 0.0, 70.0, 25.0)
 HbA1c_level = st.sidebar.slider("HbA1c Level", 0.0, 15.0, 6.0)
 blood_glucose_level = st.sidebar.slider("Blood Glucose Level", 0, 500, 100)
@@ -81,6 +63,7 @@ if st.sidebar.button("Predict"):
     }
     prediction = predict(input_data)
     st.success(f"The risk of you getting diabetes is {prediction:.2f}%")
+
 
 
 
